06ec31b6dfb1e3b0de9ca417ff085c4d7e9a78a4
EMcConnell20 <ethan.mcconnell20@gmail.com>
2023-12-29 07:13:54 -0600

    Finished FinderFunctions Module
    FinderFunctions is now fully functional. The 'scanFolder' function can now be used to return a list of directory paths of files with duplicate names in a nested folder structure.

--- FinderFunctions.py
+++ FinderFunctions.py
@@ -47,6 +47,30 @@ def hashFolderContents(
     return file_hash_list
 
 
+def createFilePathList(file_hash: "int", hash_parent_paths: "list") -> "list":
+    """
+    Scans folders for hashed file names that match 'file_hash'.
+    Returns a list of the directory paths of those files.
+
+    Args:
+        file_hash (int): The hash value to search for.
+        hash_parent_paths (list): The parent folders of the duplicate names.
+    """
+    # The directory paths of files with duplicate hashes.
+    file_path_list = []
+
+    # Checks each folder in 'hash_parent_paths' for a file name with the hash value 'file_hash'.
+    for folder_path in hash_parent_paths:
+        file_name_list = os.listdir(folder_path)
+
+        for file_name in file_name_list:
+            if hash(file_name) == file_hash:
+                file_path_list.append(os.path.join(folder_path, file_name))
+                break
+
+    return file_path_list
+
+
 def scanFolder(root_path: "str") -> "dict":
     """
     Scans a nested folder structure for files with matching names.
@@ -70,8 +94,7 @@ def scanFolder(root_path: "str") -> "dict":
     # Every hashed file name in the 'root_path' nested folder structure.
     total_hash_list = []
 
-    # Indexes of 'total_hash_list' that denote the parent folders of subsequent values.
-    # 'total_hash_list[]
+    # Indexes of 'total_hash_list' that denote the parent folders of subsequent values in that list.
     total_folder_index_list = []
 
     # Collects hashed file names, stores their position, and removes any empty or unaccessible folder paths from 'folder_path_list'.
@@ -86,14 +109,81 @@ def scanFolder(root_path: "str") -> "dict":
         else:  # If the folder was unaccessible or didn't contain any files.
             folder_path_list.remove(folder_path)
 
-    #
+    # Stores all duplicate hashes.
     duplicate_hash_list = []
+
+    # Stores the parent folder paths of all duplicate hashes.
     duplicate_hash_paths_list = []
 
-    #
+    # Stores the length of 'total_hash_list' so that the loop doesn't have to check it every single time.
     hash_list_length = len(hash_list)
-    current_item_index = 0
 
-    #
+    # Finds duplicate hashes and creates nested lists of directory paths that the hash came from.
     while hash_list_length > 0:
-        current_item = total_hash_list[current_item_index]
+        current_item = total_hash_list[0]
+        item_count = total_hash_list.count(current_item)
+
+        if item_count > 0:
+            folder_index = 0
+            path_list = []
+
+            # Creates a list of folder paths that 'current_item' is in.
+            while item_count > 0:
+                item_index = total_hash_list.index(current_item)
+
+                while (
+                    folder_index < len(total_folder_index_list) - 1
+                    and item_index >= total_folder_index_list[folder_index + 1]
+                ):
+                    folder_index += 1
+
+                path_list.append(folder_path_list[folder_index])
+                total_hash_list.remove(current_item)
+                item_count -= 1
+                hash_list_length -= 1
+
+                for iterator in range(folder_index + 1, len(total_folder_index_list)):
+                    if iterator != 0:
+                        total_folder_index_list[iterator] -= 1
+
+                        if (
+                            total_folder_index_list[iterator]
+                            == total_folder_index_list[iterator - 1]
+                        ):
+                            total_folder_index_list.pop(iterator - 1)
+                            folder_path_list.pop(iterator - 1)
+
+            duplicate_hash_list.append(current_item)
+            duplicate_hash_paths_list.append(path_list)
+        else:
+            total_hash_list.remove(current_item)
+            hash_list_length -= 1
+
+            for iterator in len(total_folder_index_list):
+                if iterator != 0:
+                    total_folder_index_list[iterator] -= 1
+
+                    if iterator == 1 and total_folder_index_list[iterator] == 0:
+                        total_folder_index_list.pop(iterator - 1)
+                        folder_path_list.pop(iterator - 1)
+
+    # Stores the directory path of every file with duplicate names.
+    duplicate_directory_path_list = []
+
+    # Finds and records the directory path of every file with a hash in 'duplicate_hash_list'.
+    for iterator in range(len(duplicate_hash_list)):
+        for directory_path in createFilePathList(
+            duplicate_hash_list[iterator], duplicate_hash_paths_list[iterator]
+        ):
+            duplicate_directory_path_list.append(directory_path)
+
+    # End
+    return duplicate_directory_path_list
+
+
+if __name__ == "__main__":
+    os.system("clear")
+    root_path = input("Root Folder Path: ")
+
+    path_list = scanFolder(root_path)
+    print(f"\n{path_list}")
